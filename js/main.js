9a8da8d1a99c3be787630616a28c44ac6249df0e
Freddy Guerra <arcobaleno262@gmail.com>
2022-07-17 23:52:35 -0300

    termine boton, problemas con la ip

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 const btnSwitch = document.querySelector("#switch");
+let btnMagic = document.getElementById("btnMagic");
 
-btnSwitch.addEventListener("click", () => {
-  document.head.state;
+btnSwitch.addEventListener("click", function () {
+  if (btnMagic.href == "http://127.0.0.1:5503/css/estilos.css") {
+    btnMagic.href = "css/estilos-retro.css";
+  } else if (btnMagic.href == "http://127.0.0.1:5503/css/estilos-retro.css") {
+    btnMagic.href = "css/estilos-futuro.css";
+  } else {
+    btnMagic.href = "css/estilos.css";
+  }
 });
